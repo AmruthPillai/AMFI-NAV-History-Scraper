c5ec1c81625706360a57c4cbec7d0c7ee3b5a1f0
Amruth Pillai <im.amruth@gmail.com>
2019-02-15 14:12:31 +0530

    initial commit

---
+++ _variables.py
@@ -0,0 +1,250 @@
+amc_data = [
+  {
+    'code': 3,
+    'name': 'Aditya Birla Sun Life Mutual Fund'
+  },
+  {
+    'code': 53,
+    'name': 'Axis Mutual Fund'
+  },
+  {
+    'code': 4,
+    'name': 'Baroda Mutual Fund'
+  },
+  {
+    'code': 36,
+    'name': 'Benchmark Mutual Fund'
+  },
+  {
+    'code': 59,
+    'name': 'BNP Paribas Mutual Fund'
+  },
+  {
+    'code': 46,
+    'name': 'BOI AXA Mutual Fund'
+  },
+  {
+    'code': 32,
+    'name': 'Canara Robeco Mutual Fund'
+  },
+  {
+    'code': 60,
+    'name': 'Daiwa Mutual Fund'
+  },
+  {
+    'code': 31,
+    'name': 'DBS Chola Mutual Fund'
+  },
+  {
+    'code': 38,
+    'name': 'Deutsche Mutual Fund'
+  },
+  {
+    'code': 58,
+    'name': 'DHFL Pramerica Mutual Fund'
+  },
+  {
+    'code': 6,
+    'name': 'DSP Mutual Fund'
+  },
+  {
+    'code': 47,
+    'name': 'Edelweiss Mutual Fund'
+  },
+  {
+    'code': 54,
+    'name': 'Essel Mutual Fund'
+  },
+  {
+    'code': 40,
+    'name': 'Fidelity Mutual Fund'
+  },
+  {
+    'code': 51,
+    'name': 'Fortis Mutual Fund'
+  },
+  {
+    'code': 27,
+    'name': 'Franklin Templeton Mutual Fund'
+  },
+  {
+    'code': 8,
+    'name': 'GIC Mutual Fund'
+  },
+  {
+    'code': 49,
+    'name': 'Goldman Sachs Mutual Fund'
+  },
+  {
+    'code': 9,
+    'name': 'HDFC Mutual Fund'
+  },
+  {
+    'code': 37,
+    'name': 'HSBC Mutual Fund'
+  },
+  {
+    'code': 20,
+    'name': 'ICICI Prudential Mutual Fund'
+  },
+  {
+    'code': 57,
+    'name': 'IDBI Mutual Fund'
+  },
+  {
+    'code': 48,
+    'name': 'IDFC Mutual Fund'
+  },
+  {
+    'code': 68,
+    'name': 'IIFCL Mutual Fund (IDF)'
+  },
+  {
+    'code': 62,
+    'name': 'IIFL Mutual Fund'
+  },
+  {
+    'code': 11,
+    'name': 'IL&FS Mutual Fund'
+  },
+  {
+    'code': 65,
+    'name': 'IL&FS Mutual Fund (IDF)'
+  },
+  {
+    'code': 63,
+    'name': 'Indiabulls Mutual Fund'
+  },
+  {
+    'code': 14,
+    'name': 'ING Mutual Fund'
+  },
+  {
+    'code': 42,
+    'name': 'Invesco Mutual Fund'
+  },
+  {
+    'code': 70,
+    'name': 'ITI Mutual Fund'
+  },
+  {
+    'code': 16,
+    'name': 'JM Financial Mutual Fund'
+  },
+  {
+    'code': 43,
+    'name': 'JPMorgan Mutual Fund'
+  },
+  {
+    'code': 17,
+    'name': 'Kotak Mahindra Mutual Fund'
+  },
+  {
+    'code': 56,
+    'name': 'L&T Mutual Fund'
+  },
+  {
+    'code': 18,
+    'name': 'LIC Mutual Fund'
+  },
+  {
+    'code': 69,
+    'name': 'Mahindra Mutual Fund'
+  },
+  {
+    'code': 45,
+    'name': 'Mirae Asset Mutual Fund'
+  },
+  {
+    'code': 19,
+    'name': 'Morgan Stanley Mutual Fund'
+  },
+  {
+    'code': 55,
+    'name': 'Motilal Oswal Mutual Fund'
+  },
+  {
+    'code': 44,
+    'name': 'PineBridge Mutual Fund'
+  },
+  {
+    'code': 34,
+    'name': 'PNB Mutual Fund'
+  },
+  {
+    'code': 64,
+    'name': 'PPFAS Mutual Fund'
+  },
+  {
+    'code': 10,
+    'name': 'Principal Mutual Fund'
+  },
+  {
+    'code': 13,
+    'name': 'Quant Mutual Fund'
+  },
+  {
+    'code': 41,
+    'name': 'Quantum Mutual Fund'
+  },
+  {
+    'code': 21,
+    'name': 'Reliance Mutual Fund'
+  },
+  {
+    'code': 35,
+    'name': 'Sahara Mutual Fund'
+  },
+  {
+    'code': 22,
+    'name': 'SBI Mutual Fund'
+  },
+  {
+    'code': 52,
+    'name': 'Shinsei Mutual Fund'
+  },
+  {
+    'code': 67,
+    'name': 'Shriram Mutual Fund'
+  },
+  {
+    'code': 66,
+    'name': 'SREI Mutual Fund (IDF)'
+  },
+  {
+    'code': 2,
+    'name': 'Standard Chartered Mutual Fund'
+  },
+  {
+    'code': 24,
+    'name': 'SUN F&C Mutual Fund'
+  },
+  {
+    'code': 33,
+    'name': 'Sundaram Mutual Fund'
+  },
+  {
+    'code': 25,
+    'name': 'Tata Mutual Fund'
+  },
+  {
+    'code': 26,
+    'name': 'Taurus Mutual Fund'
+  },
+  {
+    'code': 61,
+    'name': 'Union Mutual Fund'
+  },
+  {
+    'code': 28,
+    'name': 'UTI Mutual Fund'
+  },
+  {
+    'code': 71,
+    'name': 'YES Mutual Fund'
+  },
+  {
+    'code': 29,
+    'name': 'Zurich India Mutual Fund'
+  }
+]
---
+++ script.py
@@ -0,0 +1,88 @@
+# Imports
+import requests
+from datetime import datetime
+from _variables import amc_data
+from dateutil.relativedelta import relativedelta
+from pymongo import MongoClient, ASCENDING, DESCENDING
+
+# Scheme Code;Scheme Name;ISIN Div Payout/ISIN Growth;ISIN Div Reinvestment;Net Asset Value;Repurchase Price;Sale Price;Date
+# 101711;Aditya Birla Sun Life MIP II - Savings 5 Plan - Monthly Dividend - Regular Plan;INF209K01736;INF209K01DE3;11.2839;11.1711;11.2839;04-Jan-2010
+
+# Constants
+url = 'http://portal.amfiindia.com/DownloadNAVHistoryReport_Po.aspx'
+start_date_str = '01-Jan-2010'
+mongodb_host = 'localhost'
+mongodb_port = 27017
+
+# Exceptions
+class DuplicateNAVEntry(Exception):
+  pass
+duplicate_nav_entry = DuplicateNAVEntry()
+
+# To generate dates
+start_date = datetime.strptime(start_date_str, '%d-%b-%Y')
+today = datetime.today()
+
+dates = []
+
+x = start_date
+while x < today:
+  x = x + relativedelta(months=1)
+  dates.append(x.strftime('%d-%b-%Y'))
+
+# Connect to MongoDB
+client = MongoClient(mongodb_host, mongodb_port)
+db = client['amfi']
+collection = db['nav_history']
+collection.create_index([('scheme_code', ASCENDING), ('date', DESCENDING)])
+cursor = collection.find({})
+
+# Iterate through AMC List
+for amc in amc_data:
+  print('\n--------------------')
+  print('Scraping', amc['name'])
+
+  # Iterate through dates to get previous and current date
+  for idx, date in enumerate(dates):
+    if idx == 0: continue
+    print('From', dates[idx - 1], 'to', dates[idx])
+
+    params = {
+      'mf': amc['code'],
+      'frmdt': dates[idx - 1],
+      'todt': dates[idx]
+    }
+
+    res = requests.get(url, params)
+    data = res.text
+
+    # If it does not contain 'Scheme Code' as the first entry, then data does not exist
+    if not data.startswith('Scheme'):
+      print('-- No data found for this time period!')
+      continue
+
+    data_lines = data.splitlines()
+    data_lines = data_lines[1:]
+    for line in data_lines:
+      arr_points = []
+      x = line.split(';')
+
+      # Process the line only if it has relevant fields
+      if len(x) == 8:
+        try:
+          for doc in cursor:
+            if (doc['scheme_code'] == int(x[0]) and doc['timestamp'] == datetime.strptime(x[7], '%d-%b-%Y')):
+              raise duplicate_nav_entry
+
+          point = dict()
+          point['amc_code'] = amc['code']
+          point['amc_name'] = amc['name']
+          point['scheme_code'] = int(x[0])
+          point['scheme_name'] = x[1]
+          point['isin'] = x[2]
+          point['nav'] = float(x[4])
+          point['timestamp'] = datetime.strptime(x[7], '%d-%b-%Y')
+
+          collection.insert_one(point)
+        except DuplicateNAVEntry:
+          continue
